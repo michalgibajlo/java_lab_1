2583e992d8651dbf2294f159b3748ced83289b41
michalgibajlo <michal.gibajlo@gmail.com>
2021-11-21 19:58:27 +0100

    Dodany tekst

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,44 @@ public class Main {
         /* ćw.2 */
 
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
+        int n = 1;
+        int x,y, wynik;
+        String c = "Kasia";
+        double d = 1.0;
+        double e,f, wynikdouble;
+        long l = 9;
+        //    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
+        //    mnożenia, dziealenia i modulo na zmiennych:
+        //   a) liczbaA i liczbaB, dowolne liczby calkowite (int),
 
-        /*
-        *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
-        *    mnożenia, dziealenia i modulo na zmiennych:
-        *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
-        *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
-        *  */
+        //LICZBY:
+        x=7;
+        y=12;
 
+        wynik = x+y;
+        System.out.println("Dodawanie: " + wynik );
+        wynik = x-y;
+        System.out.println("Odejmowanie: " + wynik );
+        wynik = x*y;
+        System.out.println("Mnożenie: " + wynik );
+        wynik = x/y;
+        System.out.println("Dzielenie: " + wynik );
+        wynik = x%y;
+        System.out.println("Modulo: " + wynik );
+        //   b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
+        e=7;
+        f=12;
+
+        wynikdouble = e+f;
+        System.out.println("Dodawanie: " + wynikdouble );
+        wynikdouble = e-f;
+        System.out.println("Odejmowanie: " + wynikdouble );
+        wynikdouble = e*f;
+        System.out.println("Mnożenie: " + wynikdouble );
+        wynikdouble = e/f;
+        System.out.println("Dzielenie: " + wynikdouble );
+        wynikdouble = e%f;
+        System.out.println("Modulo: " + wynikdouble );
 
     }
 }
