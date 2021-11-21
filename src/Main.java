b85ebd048f20f7d91a93e895b6f3dd410379feff
michalgibajlo <michal.gibajlo@gmail.com>
2021-11-21 19:36:32 +0100

    Dodany tekst

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
-
+        System.out.println("Tekst w konsoli");
+        System.out.print("to też tekst ale bez entera na końcu");
     }
 }
