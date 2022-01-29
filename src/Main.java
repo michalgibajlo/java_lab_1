52c29f18a944fe31d2cbe0acc7d9f89f8e28e7d7
michalgibajlo <michal.gibajlo@gmail.com>
2022-01-30 00:32:13 +0100

    Dodany tekst

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import com.sun.source.tree.IfTree;
+
 import java.util.Scanner;
 
 public class Main {
@@ -10,13 +12,36 @@ public class Main {
          * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
          * */
 
+        int age = 27;
+        if (age%3==0 )
+        {
+            System.out.println("Wiek podzielny przez 3");
+        }
+        else
+        {
+            System.out.println("Wiek niepodzielny przez 3");
+        }
+
 
         /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
 
+        int indeks=68635;
+
+        String modulo=indeks%2==0 ? "Parzyste" : "Nieparzyste";
+        System.out.println(modulo);
 
         /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
          * i stwórz konstrukcję else if z dowolnymi komunikatami */
 
+        System.out.print("wprowadź liczbę:");
+        Scanner scan_liczba = new Scanner(System.in);
+        String liczba = scan_liczba.nextLine();
+
+        Double liczbaDB = Double.parseDouble(liczba);
+
+        String pytanie=liczbaDB>=0 ? "Większa od zera" : "Mniejsza od zera";
+
+        System.out.println(pytanie);
 
     }
 }
