83465ec63c0dd02a6ede18b19f88e10d51b780b6
michalgibajlo <michal.gibajlo@gmail.com>
2022-01-30 14:40:28 +0100

    Dodany tekst

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -9,6 +7,19 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+        Double[] tab1 = new Double[5];
+
+        tab1[0] = 29.09;
+        tab1[1] = 21.00;
+        tab1[2] = 3.00;
+        tab1[3] = 100.50;
+        tab1[4] = 1.98;
+
+        //System.out.println(tab1.length);
+        for (int i = 0; i < tab1.length; i++)
+        {
+            System.out.print(tab1[i] + "; ");
+        }
 
     }
 }
